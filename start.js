342f1f034696e92afe547e2ab34bfb61c6fa39cb
adeliofioritto <adelio.fioritto@gmail.com>
2023-11-21 13:19:31 +0100

    Initial commit

---
+++ start.js
@@ -0,0 +1,21 @@
+// Require library
+var excel = require('excel4node');
+const oracledb = require('oracledb');
+const nodemailer = require('nodemailer');
+fs = require('fs');
+var CronJob = require('cron').CronJob;
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const port = 3000;
+
+
+
+app.listen(port, () => console.log(`STATS is listening on port ${port}!`))
+
+console.log("TEST STARTED");
